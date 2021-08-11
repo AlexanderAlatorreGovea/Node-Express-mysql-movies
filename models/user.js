1e5e01037b8c76489bd0e6809922b2513c555e84
Alexander Govea <alexander_alatorre1993@hotmail.com>
2021-08-10 20:06:54 -0600

    added files for migration.

---
+++ models/user.js
@@ -0,0 +1,51 @@
+"use strict";
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Account }) {
+      // define association here
+      this.hasOne(Account);
+
+    }
+  }
+  User.init(
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: { msg: 'User must have a name' },
+          notEmpty: { msg: 'Name must not be empty' },
+        },
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: { msg: 'User must have a last name' },
+          notEmpty: { msg: 'Last name must not be empty' },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false, 
+        validate: {
+          notNull: { msg: 'User must have an email'},
+          notNull: { msg: 'Email must not be empty'},
+          isEmail: { msg: 'Must be a valid email address' },
+        }
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  return User;
+};
